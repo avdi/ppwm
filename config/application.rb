6bfd12e22a8d22b7be7782831b692156e7ea3122
Avdi Grimm <avdi@avdi.org>
2013-06-13 12:33:51 -0400

    Add ppwm-matcher.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Ppwm
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.paths["db/migrate"] += PpwmMatcher::Engine.paths["db/migrate"].existent
   end
 end
